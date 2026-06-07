2e2fa48fb31f33be30debd0a3a23931ef622b509
RanjeethReddy29g <gouniranjeethreddy29@gmail.com>
2026-06-07 12:24:51 +0530

    checking pipeline

--- src/main/java/com/game/game2048/Game2048Application.java
+++ src/main/java/com/game/game2048/Game2048Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Game2048Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(Game2048Application.class, args);
 	}
 
